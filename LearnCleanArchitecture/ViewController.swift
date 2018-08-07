6daadddeb5055795c5666c075752c645857312e3
tran.xuan.thien <tran.xuan.thien@b120667-mb.local>
2018-08-07 09:18:36 +0700

    Initial Commit

---
+++ LearnCleanArchitecture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearnCleanArchitecture
+//
+//  Created by tran.xuan.thien on 8/7/18.
+//  Copyright © 2018 tran.xuan.thien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
